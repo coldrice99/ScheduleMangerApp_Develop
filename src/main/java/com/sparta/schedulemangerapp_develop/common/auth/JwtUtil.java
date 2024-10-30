8dfe6418219b740bf6369ddc5a4d79514eb4e0c1
coldrice99 <coldrice99@gmail.com>
2024-10-30 10:38:12 +0900

    필터 구현

--- src/main/java/com/sparta/schedulemangerapp_develop/common/auth/JwtUtil.java
+++ src/main/java/com/sparta/schedulemangerapp_develop/common/auth/JwtUtil.java
@@ -17,7 +17,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.security.Key;
-import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -84,22 +83,22 @@ public class JwtUtil {
         throw new NullPointerException("Not Found Token");
     }
 
-//    // JWT 검증
-//    public boolean validateToken(String token) {
-//        try {
-//            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-//            return true;
-//        } catch (SecurityException | MalformedJwtException | SignatureException e) {
-//            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
-//        } catch (ExpiredJwtException e) {
-//            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
-//        } catch (UnsupportedJwtException e) {
-//            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
-//        } catch (IllegalArgumentException e) {
-//            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
-//        }
-//        return false;
-//    }
+    // JWT 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
 
     // 토큰에서 사용자 정보 가져오기
     public Claims getUserInfoFromToken(String token) {
